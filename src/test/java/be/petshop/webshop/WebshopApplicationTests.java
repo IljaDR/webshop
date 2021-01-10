ef76a4d3a4fdb715418db073923eed24fa18538a
IljaDR <38908439+IljaDR@users.noreply.github.com>
2021-01-10 23:28:15 +0100

    Initial commit

---
+++ src/test/java/be/petshop/webshop/WebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package be.petshop.webshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
